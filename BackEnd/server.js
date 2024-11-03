99f0f65bfb112fa6ab3e285d5dda49c52d939894
vansh jaiswal <vanshjais1777@gmail.com>
2024-11-03 17:47:37 +0530

    last commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,12 +5,16 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173'  // Allow requests from this origin
-}));
-
-app.use(express.json());
+    origin: ['http://localhost:5173'], // local frontend and admin frontend origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+    credentials: true,
+  }));
+  
+  app.options('*', cors());
 
 const connectDB = require('./config/db')
 connectDB();
